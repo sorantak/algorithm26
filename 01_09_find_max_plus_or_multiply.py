52a1fb93df095e0feef897415ef0593faae02aff
sorantak <hansoltak@gmail.com>
2026-01-09 17:59:11 +0900

    :zap: Is number exist

---
+++ 01_09_find_max_plus_or_multiply.py
@@ -0,0 +1,9 @@
+def find_max_plus_or_multiply(array):
+    # 이 부분을 채워보세요!
+    return 1
+
+
+result = find_max_plus_or_multiply
+print("정답 = 728 현재 풀이 값 =", result([0,3,5,6,1,2,4]))
+print("정답 = 8820 현재 풀이 값 =", result([3,2,1,5,9,7,4]))
+print("정답 = 270 현재 풀이 값 =", result([1,1,1,3,3,2,5]))
